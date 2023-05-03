0d2864fe5e164ac3712775091682bf6b87745fd0
Crowdbotics <team@crowdbotics.com>
2023-05-03 23:59:24 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "the-meeker-guide-40434",
+  url: "https://the-meeker-guide-40434.botics.co",
+  api: "https://the-meeker-guide-40434.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
